afb25c494b9f57bfabf6be46d52b45623c5cac34
nmthong226 <nmthong226@gmail.com>
2024-08-28 23:25:59 +0700

    add component

---
+++ src/components/User.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Users = ({ users, onChat }) => {
+  const [activeUser, setActiveUser] = useState(null);
+
+  const handleChat = (id) => {
+    setActiveUser(id);
+    onChat(id);
+  };
+
+  return (
+    <div className="mt-0">
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className={`user ${activeUser === user.id ? "active" : ""}`}
+          onClick={() => handleChat(user.id)}
+        >
+          {user.userName}
+          {user.has_new_message && (
+            <span className="has_new_message">New message</span>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Users;
