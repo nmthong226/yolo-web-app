f1f6d2f2cdc356f54afcd9bdb3f8349b9f3ac0bf
nmthong226 <nmthong226@gmail.com>
2024-08-29 23:33:16 +0700

    add pkg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import envCompatible from 'vite-plugin-env-compatible'
+import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  define: {
+    'process.env.VITE_API_BASE': JSON.stringify(process.env.VITE_API_BASE),
+  }
 })
