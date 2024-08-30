c709ac71d298d1113868b3c7db69a816dbad7682
nmthong226 <nmthong226@gmail.com>
2024-08-30 22:58:41 +0700

    change home page ui

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import NavBar from './NavBar';
 import Users from './User';
 import Messages from './Messages/Messages';
 import MessageInput from './MessageInput';
+import logo from '/public/logo.png';
+import { FaPlus } from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
+import { getColor, createImageFromInitials } from '../utils/userAvatar.js';
+import { GoQuestion } from "react-icons/go";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const HomePage = ({ loggedUserUsername }) => {
     const [loadingModel, setLoadingModel] = useState(false);
@@ -12,8 +18,13 @@ const HomePage = ({ loggedUserUsername }) => {
     const [currentChatChannel, setCurrentChatChannel] = useState(null);
     const [pusher, setPusher] = useState(null);
     const [token, setToken] = useState("");
-
-    // Your existing methods like handleChat, getMessage, etc.
+    console.log(loggedUserUsername);
+    const classNames = (...classes) => {
+        return classes.filter(Boolean).join(' ');
+    }
+    const logout = () => {
+        console.log("logout");
+    };
     const getMessage = (channelName) => {
         axios
             .get(`/api/get_message/${channelName}`, {
@@ -30,13 +41,11 @@ const HomePage = ({ loggedUserUsername }) => {
     const handleChat = (id) => {
         setActiveChatId(id);
         const activeChatIndex = users.findIndex((user) => user.id === id);
-
         setUsers((prevUsers) =>
             prevUsers.map((user, i) =>
                 i === activeChatIndex ? { ...user, has_new_message: false } : user
             )
         );
-
         setLoadingModel(true);
         axios
             .post(
@@ -51,9 +60,7 @@ const HomePage = ({ loggedUserUsername }) => {
                 setLoadingModel(false);
                 const channelName = response.data.channel_name;
                 setCurrentChatChannel(channelName);
-
                 getMessage(channelName);
-
                 const isSubscribed = pusher.channel(channelName);
                 if (!isSubscribed) {
                     const channel = pusher.subscribe(channelName);
@@ -61,7 +68,6 @@ const HomePage = ({ loggedUserUsername }) => {
                         ...prevMessages,
                         [channelName]: [],
                     }));
-
                     channel.bind("new_message", (data) => {
                         if (data.channel !== currentChatChannel && data.from_user !== loggedUserId) {
                             setUsers((prevUsers) =>
@@ -70,7 +76,6 @@ const HomePage = ({ loggedUserUsername }) => {
                                 )
                             );
                         }
-
                         setMessages((prevMessages) => ({
                             ...prevMessages,
                             [channelName]: [
@@ -122,29 +127,70 @@ const HomePage = ({ loggedUserUsername }) => {
         });
     };
     return (
-        <div className="container mx-auto">
-            <NavBar loggedUser={loggedUserUsername} />
-            <div className="flex main-area">
-                <div className="w-1/4 p-0 users">
-                    <Users users={users} onChat={handleChat} />
+        <div className="flex flex-row h-screen">
+            <div className="flex flex-col justify-between w-1/5 h-full">
+                <div className='flex flex-row h-[8%] w-full border-2 border-gray-300 items-center'>
+                    <img src={logo} alt="logo" className="h-10 ml-4" />
+                    <div className="text-zinc-800 font-bold ml-2">Canity Image</div>
                 </div>
-                <div className="w-3/4 p-0 messages-area">
-                    <div className="messages-main overflow-y-scroll h-[90%]">
-                        {loadingModel ? (
-                            <div className="select-chat text-center">Loading model...</div>
-                        ) : !currentChatChannel ? (
-                            <div className="select-chat text-center">
-                                Select your favorite bot!
-                            </div>
-                        ) : (
-                            <Messages
-                                activeChat={activeChatId}
-                                messages={messages[currentChatChannel] || []}
-                            />
-                        )}
+                <div className='flex-grow flex flex-col border-2 border-t-0 border-gray-300'>
+                    <div className='w-full h-full'>
+                        <button className='flex flex-row px-4 w-[94%] h-[6%] ml-[3%] bg-[#484FA2] hover:bg-[#484FA2]/90 items-center mt-2 rounded-lg text-white'>
+                            <FaPlus className='text-white mr-2' />
+                            <span>New chat</span>
+                        </button>
+                        <div className='flex flex-row px-4 w-[94%] h-[6%] ml-[3%] bg-gray-200 items-center mt-2 rounded-lg text-white'>
+                            <CiSearch className='text-gray-600 mr-2' />
+                            <input
+                                className='bg-gray-200 focus:outline-none focus:ring-0 text-gray-800 h-full w-full'
+                                placeholder='Search'>
+                            </input>
+                        </div>
                     </div>
-                    <MessageInput onSendMessage={sendMessage} onSendFile={sendFile} />
+                    <div className='ml-[8%] mb-[10%] text-gray-500 space-y-[4%]'>
+                        <button className='flex items-center'>
+                            <IoSettingsOutline className='mr-2 size-5' />
+                            <span>Settings</span>
+                        </button>
+                        <button className='flex items-center'>
+                            <GoQuestion className='mr-2 size-5' />
+                            <span>Help</span>
+                        </button>
+                    </div>
                 </div>
+                <div className='flex flex-row h-[8%] border-2 border-gray-300 items-center'>
+                    <div className='flex items-center ml-[4%]'>
+                        <button className='relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:outline-none focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                            <img
+                                id='preview'
+                                src={createImageFromInitials(120, loggedUserUsername, getColor())}
+                                alt='profile-pic'
+                                className='h-10 w-10 rounded-full'
+                            />
+                        </button>
+                        <span className='ml-2'>{loggedUserUsername}</span>
+                    </div>
+                </div>
+                {/* <Users users={users} onChat={handleChat} /> */}
+            </div>
+            <div className="flex flex-col w-4/5 h-full">
+                <NavBar />
+                <div className="bg-gray-100 h-[74%]">
+                    {loadingModel ? (
+                        <div className="select-chat text-center">Loading model...</div>
+                    ) : !currentChatChannel ? (
+                        <div className="flex flex-col items-center mt-[2%] w-full h-full">
+                            <span className='text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#9372ff] via-[#484fa2] to-[#4f14ff]'>Welcome {loggedUserUsername}</span>
+                            <span className='text-2xl font-bold text-gray-400'>How can I help you?</span>
+                        </div>
+                    ) : (
+                        <Messages
+                            activeChat={activeChatId}
+                            messages={messages[currentChatChannel] || []}
+                        />
+                    )}
+                </div>
+                <MessageInput onSendMessage={sendMessage} onSendFile={sendFile} />
             </div>
         </div>
     );
