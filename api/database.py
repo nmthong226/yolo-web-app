c841cb6edbc2b613b31692e538fa3eca98d666ff
nmthong226 <nmthong226@gmail.com>
2024-09-03 17:41:18 +0700

    db init config

--- api/database.py
+++ api/database.py
@@ -6,5 +6,5 @@ db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind
 Base = declarative_base()
 Base.query = db_session.query_property()
 def init_db():
-    import models
+    import model_schema
     Base.metadata.create_all(bind=engine)
