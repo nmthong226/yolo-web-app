5f31a0b33b3416ad4ae3938ad82640d2625c453c
nmthong226 <nmthong226@gmail.com>
2024-09-05 23:36:02 +0700

    configure global

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ export default {
     extend: {
       screens: {
         "xsm": "425px",
-      }
+      },
     },
+    fontSize: {
+      'xsm': '0.75rem',
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.25rem',
+      '5xl': '2.5rem',
+    }
   },
   plugins: [],
 }
